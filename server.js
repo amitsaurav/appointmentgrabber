f9c5815dfc162f8b17cb22ca5244c87a836babea
Amit Saurav <amitsaurav@gmail.com>
2014-05-28 09:10:26 -0700

    More correction.

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ function callback(error, response, body) {
     var dataSplit = data.split('\r\n');
     var date = dataSplit[1];
     
-    if (!date) {
+    if (typeof date === "undefined" || date === null) {
       sendSMS('Invalid page received.');
       process.exit(1);
     } else if (date.indexOf('Jul') >= 0 || date.indexOf('Aug') >= 0) {
